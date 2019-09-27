538f3266dab04c6fb1eff9c85231d202edc08fda
FrankYeah <aaa24295234@gmail.com>
2019-09-27 21:08:01 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,22 @@
-# //deploy.sh
 #!/usr/bin/env sh
-# 當發生錯誤時終止腳本運行
 
+# 當發生錯誤時終止腳本運行
 set -e
 
 # 打包
-
 npm run build
 
-# 移動至到打包後的dist目錄 
-
+# cd 到打包後的dist目錄下 
 cd dist
 
-git init 
-
-# //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
-
+# # 初始化git 並 add 與 commit
+git init
+git config user.name "chou0728"
+git config user.email "dean34520@gmail.com"
 git add -A
-
 git commit -m 'deploy'
 
-# 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
-
+# 部署到 https://github.com/chou0728/vue-form.git 分支為 gh-pages
 git push -f https://github.com/frankyeah/market.git master:gh-pages
 
-# //將dist資料夾中的內容推送至遠端eric-project的gh-pages分支中，並強制無條件將舊有的內容取代成目前的內容（指令 git push -f)
-# cd -
+cd -d
